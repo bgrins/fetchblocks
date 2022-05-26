0b5da8f442164cd554838bad46a26dc5a86901b1
Brian Grinstead <briangrinstead@gmail.com>
2022-05-26 13:50:58 -0700

    update deps

--- deps.js
+++ deps.js
@@ -9,20 +9,17 @@ import { configSync } from "https://deno.land/std@0.137.0/dotenv/mod.ts";
 
 const CONFIG = configSync();
 
-console.log(CONFIG)
-
 // Set
 // USE_RELATIVE_IMPORTS_FOR_DEVELOPMENT=true
 // Todo: change this to a remote endpoint - env USE_RELATIVE_IMPORTS_FOR_DEVELOPMENT
-export const UTILS_IMPORT_BASE = CONFIG.USE_RELATIVE_IMPORTS_FOR_DEVELOPMENT === "true"
-  ? import.meta.url
-  : "https://raw.githubusercontent.com/bgrins/fetchblocks/wip/";
-
+export const UTILS_IMPORT_BASE =
+  CONFIG.USE_RELATIVE_IMPORTS_FOR_DEVELOPMENT === "true"
+    ? import.meta.url
+    : "https://raw.githubusercontent.com/bgrins/fetchblocks/wip/";
 
 // export { DOMParser } from "https://unpkg.com/linkedom/worker";
-export { DOMParser } from "https://esm.sh/linkedom";
 // export { DOMParser } from "https://esm.sh/linkedom?target=es2022&bundle";
-
+export { DOMParser } from "https://esm.sh/linkedom";
 export { jmespath };
 
 // https://github.com/ai/nanoid/:
